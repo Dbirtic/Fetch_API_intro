945efad0b7f6296a9e3b64d74e43cfe75941ee04
Dbirtic <Dominik.birtic666@gmail.com>
2021-04-01 21:37:37 +0200

    added some functions for getting data and posting data with fetch, added a bit of bootstrap for visuals

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 document.getElementById("getText").addEventListener('click', getText);
 
+document.getElementById("getUsers").addEventListener('click', getUsers);
+
+document.getElementById("getPosts").addEventListener('click', getPosts);
+
+document.getElementById("addPost").addEventListener('submit', addPost);
+
 function getText(){
     /* fetch('sample.txt')
     .then(function(res){
@@ -13,4 +19,57 @@ function getText(){
     .then((res) => res.text())
     .then((data) => {document.getElementById('output').innerHTML = data})
     .catch((err) => console.log(err));
+}
+
+function getUsers(){
+    fetch('users.json')
+    .then((res) => res.json())
+    .then((data) => {
+        let output = '<h2 class="mb-4">Users</h2>';
+        data.forEach((user) =>{
+            output += `
+                <ul>
+                    <li class="list-group-item">ID: ${user.id}</li>
+                    <li  class="list-group-item">Name: ${user.name}</li>
+                    <li  class="list-group-item">Email: ${user.email}</li>
+                </ul>
+            `;
+        });
+        document.getElementById('output').innerHTML = output;
+    })
+}
+
+function getPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((data) => {
+        let posts = '<h2 class="mb-4">Posts</h2>';
+        data.forEach((post) =>{
+            posts += `
+                <div class="card card-body mb-3">
+                    <h3>${post.title}</h3>
+                    <p>${post.body}</p>
+                </div>
+            `;
+        });
+        document.getElementById('posts').innerHTML = posts;
+    });
+}
+
+function addPost(e){
+    e.preventDefault();
+
+    let title = document.getElementById('title').value;
+    let body = document.getElementById('body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method:'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({title: title, body: body})
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 }
